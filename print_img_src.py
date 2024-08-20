475ff87332d80dae62931f63febfdb35531fdbaa
Brice Chivu <bricechivu@gmail.com>
2024-08-20 14:53:50 +0800

    python

---
+++ print_img_src.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import click
+
+
+@click.command()
+@click.option("--country")
+def print_img_src(country: str):
+    path = Path(f"assets/media/{country}").resolve()
+    if not path.exists():
+        print(f"path:{path} is wrong")
+    for file in path.glob("*"):
+        name = str(file).split("blog/")[1]
+        print(f'<img src="{name}" />')
+
+
+if __name__ == "__main__":
+    print_img_src()
