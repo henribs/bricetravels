af9d3ee1c37adae85301e40be91b30079138fa8a
Brice Chivu <bricechivu@gmail.com>
2024-08-20 16:18:18 +0800

    done until 15 aug included

--- print_img_src.py
+++ print_img_src.py
@@ -9,7 +9,7 @@ def print_img_src(country: str):
     path = Path(f"assets/media/{country}").resolve()
     if not path.exists():
         print(f"path:{path} is wrong")
-    for file in sorted(path.glob("*")):
+    for file in sorted(path.glob("*"), key=lambda x: "_".join(str(x).split("_")[2:])):
         name = str(file).split("blog/")[1]
         print(f'<img src="{name}" />')
 
