c285f3c621b4472230c6f07f93ff6981f8bae1db
Brice Chivu <bricechivu@gmail.com>
2024-10-14 20:23:03 +0900

    modified print py function

--- print_img_src.py
+++ print_img_src.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import click
+import pyperclip
 
 # Usage: python3 print_img_src.py --country=Taoyuan
 from PIL import Image
@@ -12,15 +13,17 @@ def print_img_src(country: str):
     path = Path(f"assets/media/{country}").resolve()
     if not path.exists():
         print(f"path:{path} is wrong")
+    html_ouput = ""
     for filepath in sorted(
         path.glob("*"), key=lambda x: "_".join(str(x).split("_")[2:])
     ):
         if any(ext in str(filepath).lower() for ext in ["jpg", "jpeg", "png"]):
             img = Image.open(filepath)
             name = str(filepath).split("blog/")[1]
-            print(
-                f'<img src="{name}" width="{img.width}" height="{img.height}" loading="lazy"/>'
-            )
+            html_line = f'<img src="{name}" width="{img.width}" height="{img.height}" loading="lazy"/>'
+            html_ouput += html_line + "\n"
+    print(html_ouput)
+    pyperclip.copy(html_ouput)
 
 
 if __name__ == "__main__":
